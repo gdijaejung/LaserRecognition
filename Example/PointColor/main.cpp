f19299c08a653a935eb0c6f2a5c15e0af27f0424
gdijaejung <gdijaejung@gmail.com>
2015-05-29 17:59:54 +0900

    update detect shape, point color, rect recognition

--- Example/PointColor/main.cpp
+++ Example/PointColor/main.cpp
@@ -18,6 +18,7 @@ using namespace cv;
 #pragma comment(lib, "opencv_imgproc2411d.lib")
 #pragma comment(lib, "opencv_features2d2411d.lib")
 #pragma comment(lib, "opencv_nonfree2411d.lib")
+#pragma comment(lib, "winmm.lib")
 
 
 void main()
@@ -42,32 +43,44 @@ void main()
 	cvMoveWindow("HUV05-blob", 0, 520);
 
 	int fps = 0;
+	int frameCnt = 0;
 	DWORD oldT = timeGetTime();
+	// FPS를 표시해 줄 Font 및 문자열 버퍼 초기화  
+	CvFont font;
+	cvInitFont(&font, CV_FONT_HERSHEY_COMPLEX_SMALL, 1.0, 1.0);
 
 	while (1) {
 		// check frame per seconds
-		++fps;
+		++frameCnt;
 		DWORD curT = timeGetTime();
 		if (curT - oldT > 1000)
 		{
-			printf("fps = %d\n", fps);
-			fps = 0;
+			//printf("fps = %d\n", fps);
+			fps = frameCnt;
+			frameCnt = 0;
 			oldT = curT;
 		}
 		//
 
 
-		cvGrabFrame(capture);
-		image = cvRetrieveFrame(capture);
+// 		cvGrabFrame(capture);
+// 		image = cvRetrieveFrame(capture);
+		image = cvQueryFrame(capture);
 		if (!image)
 			continue;
 
+
+		// FPS 출력.
+		char strBuffer[64] = { 0, };
+		sprintf_s(strBuffer, "%d fps", fps);
+		cvPutText(image, strBuffer, cvPoint(10, 20), &font, CV_RGB(255, 0, 255));
 		cvShowImage("HUV05-camera", image);
 
 
-		if (!output){
-			gray = cvCreateImage({640, 480}, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
-			output = cvCreateImage({640, 480}, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
+
+		if (!output) {
+			gray = cvCreateImage({ 640, 480 }, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
+			output = cvCreateImage({ 640, 480 }, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
 		}
 
 		cvCvtColor(image, gray, CV_RGB2GRAY); // 컬러를 흑백으로 변환
@@ -111,11 +124,22 @@ void main()
 		std::vector<KeyPoint> keypoints;
 		detector.detect(output, keypoints);
 
+		Mat mat(image);
+		printf("checkpoint = %d\n", keypoints.size());
+		for (u_int i = 0; i < keypoints.size(); ++i)
+		{
+			const int x = (int)keypoints[i].pt.x;
+			const int y = (int)keypoints[i].pt.y;
+
+			Vec3b *pixel = mat.ptr<Vec3b>(y);
+			printf("xy={%d,%d}, r=%d, g=%d, b=%d\n", x, y, pixel[x][2], pixel[x][1], pixel[x][0]);
+		}
+
 		// Draw detected blobs as red circles.
 		// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
 		Mat im_with_keypoints;
 		drawKeypoints(output, keypoints, im_with_keypoints, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	
+
 		//cvShowImage("HUV05-camera", image);
 		imshow("HUV05-blob", im_with_keypoints);
 	}

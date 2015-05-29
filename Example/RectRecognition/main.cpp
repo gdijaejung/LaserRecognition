c02f45400557f292c56c8ce75125b771416a6c0c
gdijaejung <gdijaejung@gmail.com>
2015-05-29 19:16:25 +0900

    update rect recognition

--- Example/RectRecognition/main.cpp
+++ Example/RectRecognition/main.cpp
@@ -20,32 +20,23 @@ using namespace cv;
 #pragma comment(lib, "opencv_nonfree2411d.lib")
 #pragma comment(lib, "winmm.lib")
 
-/**
- * Simple shape detector program.
- * It loads an image and tries to find simple shapes (rectangle, triangle, circle, etc) in it.
- * This program is a modified version of `squares.cpp` found in the OpenCV sample dir.
- */
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <cmath>
-#include <iostream>
 
 /**
- * Helper function to find a cosine of angle between vectors
- * from pt0->pt1 and pt0->pt2
- */
+* Helper function to find a cosine of angle between vectors
+* from pt0->pt1 and pt0->pt2
+*/
 static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
 {
 	double dx1 = pt1.x - pt0.x;
 	double dy1 = pt1.y - pt0.y;
 	double dx2 = pt2.x - pt0.x;
 	double dy2 = pt2.y - pt0.y;
-	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
 /**
- * Helper function to display text in the center of a contour
- */
+* Helper function to display text in the center of a contour
+*/
 void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
 {
 	int fontface = cv::FONT_HERSHEY_SIMPLEX;
@@ -57,88 +48,144 @@ void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& cont
 	cv::Rect r = cv::boundingRect(contour);
 
 	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
-	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255, 255, 255), CV_FILLED);
+	cv::putText(im, label, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
 }
 
-int main()
+
+
+void main()
 {
-	//cv::Mat src = cv::imread("polygon.png");
-	cv::Mat src = cv::imread("detect-simple-shapes-src-img.png");
-	if (src.empty())
-		return -1;
+	int threshold = 128; // 임계값(Threshold) 설정
+	IplImage* image = 0;
+	IplImage* output = 0;
+	IplImage* gray = 0;
 
-	// Convert to grayscale
-	cv::Mat gray;
-	cv::cvtColor(src, gray, CV_BGR2GRAY);
+	CvCapture* capture = cvCaptureFromCAM(0);
+	cvNamedWindow("HUV05-camera", 0);
+	cvNamedWindow("HUV05-binarization", 0);
+	cvNamedWindow("HUV05-rect", 0);
 
-	// Use Canny instead of threshold to catch squares with gradient shading
-	cv::Mat bw;
-	cv::Canny(gray, bw, 0, 50, 5);
+	cvResizeWindow("HUV05-camera", 640, 480);
+	cvResizeWindow("HUV05-binarization", 640, 480);
+	cvResizeWindow("HUV05-rect", 640, 480);
+	cvCreateTrackbar("Threthold", "HUV05-camera", &threshold, 255, NULL); // "HUV05-camera" 윈도우에 bar 를 붙이고 
 
-	// Find contours
-	std::vector<std::vector<cv::Point> > contours;
-	cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	cvMoveWindow("HUV05-camera", 0, 0);
+	cvMoveWindow("HUV05-binarization", 660, 0);
+	cvMoveWindow("HUV05-rect", 0, 520);
 
-	std::vector<cv::Point> approx;
-	cv::Mat dst = src.clone();
+	int fps = 0;
+	int frameCnt = 0;
+	DWORD oldT = timeGetTime();
+	// FPS를 표시해 줄 Font 및 문자열 버퍼 초기화  
+	CvFont font;
+	cvInitFont(&font, CV_FONT_HERSHEY_COMPLEX_SMALL, 1.0, 1.0);
 
-	for (int i = 0; i < contours.size(); i++)
-	{
-		// Approximate contour with accuracy proportional
-		// to the contour perimeter
-		cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
+	while (1) {
+		// check frame per seconds
+		++frameCnt;
+		DWORD curT = timeGetTime();
+		if (curT - oldT > 1000)
+		{
+			//printf("fps = %d\n", fps);
+			fps = frameCnt;
+			frameCnt = 0;
+			oldT = curT;
+		}
+		//
 
-		// Skip small or non-convex objects 
-		if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
+		// 		cvGrabFrame(capture);
+		// 		image = cvRetrieveFrame(capture);
+		image = cvQueryFrame(capture);
+		if (!image)
 			continue;
 
-		if (approx.size() == 3)
-		{
-			setLabel(dst, "TRI", contours[i]);    // Triangles
+		// FPS 출력.
+		char strBuffer[64] = { 0, };
+		sprintf_s(strBuffer, "%d fps", fps);
+		cvPutText(image, strBuffer, cvPoint(10, 20), &font, CV_RGB(255, 0, 255));
+		cvShowImage("HUV05-camera", image);
+
+		if (!output) {
+			gray = cvCreateImage({ 640, 480 }, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
+			output = cvCreateImage({ 640, 480 }, IPL_DEPTH_8U, 1); // 흑백 이미지 생성
 		}
-		else if (approx.size() >= 4 && approx.size() <= 6)
+
+ 		cvCvtColor(image, gray, CV_RGB2GRAY); // 컬러를 흑백으로 변환
+ 
+		if (cvWaitKey(10) >= 0)
+			break;
+
+		// 영상의 각 픽셀(x,y) 값이 threshold 값의 초과는 255 로, 그 이하는 0 으로 변환
+		cvThreshold(gray, output, threshold, 255, CV_THRESH_BINARY);
+		output->origin = image->origin; // 방향이 뒤집어 진것을 바로 잡아줌
+		cvShowImage("HUV05-binarization", output);
+
+
+
+		Mat src(image);
+		Mat grayOut(output);
+
+		// Use Canny instead of threshold to catch squares with gradient shading
+		cv::Mat bw;
+		cv::Canny(grayOut, bw, 0, 50, 5);
+
+		// Find contours
+		std::vector<std::vector<cv::Point> > contours;
+		cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+		std::vector<cv::Point> approx;
+		cv::Mat dst = src.clone();
+
+		for (u_int i = 0; i < contours.size(); i++)
 		{
-			// Number of vertices of polygonal curve
-			int vtc = approx.size();
+			// Approximate contour with accuracy proportional
+			// to the contour perimeter
+			cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
 
-			// Get the cosines of all corners
-			std::vector<double> cos;
-			for (int j = 2; j < vtc+1; j++)
-				cos.push_back(angle(approx[j%vtc], approx[j-2], approx[j-1]));
+			// Skip small or non-convex objects 
+			if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
+				continue;
 
-			// Sort ascending the cosine values
-			std::sort(cos.begin(), cos.end());
+			if (approx.size() >= 4)
+			{
+				// Number of vertices of polygonal curve
+				int vtc = approx.size();
 
-			// Get the lowest and the highest cosine
-			double mincos = cos.front();
-			double maxcos = cos.back();
+				// Get the cosines of all corners
+				std::vector<double> cos;
+				for (int j = 2; j < vtc + 1; j++)
+					cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
 
-			// Use the degrees obtained above and the number of vertices
-			// to determine the shape of the contour
-			if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3)
-				setLabel(dst, "RECT", contours[i]);
-			else if (vtc == 5 && mincos >= -0.34 && maxcos <= -0.27)
-				setLabel(dst, "PENTA", contours[i]);
-			else if (vtc == 6 && mincos >= -0.55 && maxcos <= -0.45)
-				setLabel(dst, "HEXA", contours[i]);
+				// Sort ascending the cosine values
+				std::sort(cos.begin(), cos.end());
+
+				// Get the lowest and the highest cosine
+				double mincos = cos.front();
+				double maxcos = cos.back();
+
+				// Use the degrees obtained above and the number of vertices
+				// to determine the shape of the contour
+				if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3)
+				{
+					setLabel(dst, "RECT", contours[i]);
+
+					line(dst, approx[0], approx[1], Scalar(255, 0, 0), 2);
+					line(dst, approx[1], approx[2], Scalar(255, 0, 0), 2);
+					line(dst, approx[2], approx[3], Scalar(255, 0, 0), 2);
+					line(dst, approx[3], approx[0], Scalar(255, 0, 0), 2);
+				}
+			}
 		}
-		else
-		{
-			// Detect and label circles
-			double area = cv::contourArea(contours[i]);
-			cv::Rect r = cv::boundingRect(contours[i]);
-			int radius = r.width / 2;
 
-			if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-			    std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
-				setLabel(dst, "CIR", contours[i]);
-		}
+
+		cv::imshow("dst", dst);
+		imshow("HUV05-rect", dst);
 	}
 
-	cv::imshow("src", src);
-	cv::imshow("dst", dst);
-	cv::waitKey(0);
-	return 0;
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("HUV05-camera");
+	cvDestroyWindow("HUV05-binarization");
+	cvDestroyWindow("HUV05-rect");
 }
-
